7560672773ec715665cabb233dbc2f21a3914e48
Hassen Mabrouk <hassen.mabrouk@esprit.tn>
2022-04-22 03:59:22 +0100

    Project structure && Integration template login&register

---
+++ brailie/footer.php
@@ -0,0 +1,27 @@
+
+</div>
+<script src="style/js/jquery.min.js"></script>
+<script src="style/js/popper.min.js"></script>
+<script src="style/js/bootstrap.min.js"></script>
+<script src="style/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="style/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="style/revolution/revolution-addons/filmstrip/js/revolution.addon.filmstrip.min.js"></script>
+<script src="style/revolution/revolution-addons/typewriter/js/revolution.addon.typewriter.min.js"></script>
+<script src="style/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="style/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="style/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="style/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="style/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="style/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="style/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="style/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="style/revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script src="style/js/plugins.js"></script>
+<script src="style/js/scripts.js"></script>
+<script>
+    $.backstretch([
+        "style/images/art/bg22.jpg"
+    ], {duration: 5000, fade: 1000});
+</script>
+</body>
+</html>
---
+++ brailie/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="text" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="style/images/favicon.png">
+    <title>Brailie</title>
+    <link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style/css/plugins.css">
+    <link rel="stylesheet" type="text/css" href="style/revolution/css/settings.css">
+    <link rel="stylesheet" type="text/css" href="style/revolution/css/layers.css">
+    <link rel="stylesheet" type="text/css" href="style/revolution/css/navigation.css">
+    <link rel="stylesheet" type="text/css" href="style/revolution/revolution-addons/filmstrip/css/revolution.addon.filmstrip.css">
+    <link rel="stylesheet" type="text/css" href="style/revolution/revolution-addons/typewriter/css/typewriter.css">
+    <link rel="stylesheet" type="text/css" href="style/type/icons.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style/css/color/aqua.css">
+    <link rel="stylesheet" type="text/css" href="style/css/font/font6.css">
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans+Condensed:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+</head>
+<body class="onepage" data-spy="scroll" data-target=".navbar">
+<div class="content-wrapper">
+    <nav class="navbar transparent absolute nav-wrapper-dark inverse-text navbar-expand-lg text-uppercase">
+        <div class="container">
+            <div class="navbar-header">
+                <div class="navbar-brand"><a href="index.html"><img src="#" srcset="style/images/logo-light.png 1x, style/images/logo-light@2x.png 2x" alt="" /></a></div>
+                <div class="navbar-hamburger ml-auto d-lg-none d-xl-none"><button class="hamburger animate" data-toggle="collapse" data-target=".navbar-collapse"><span></span></button></div>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link scroll" href="#home">Home</a></li>
+                    <li class="nav-item"><a class="nav-link scroll" href="#services">Services</a></li>
+                    <li class="nav-item"><a class="nav-link scroll" href="#portfolio">Portfolio</a></li>
+                    <li class="nav-item"><a class="nav-link scroll" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link scroll" href="#contact">Contact</a></li>
+                </ul>
+                <div class="navbar-divider"></div>
+            </div>
+        </div>
+    </nav>
